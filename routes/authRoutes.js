b2146d3b2c140f352435dec3776cc231dabe2da9
Yaacov Salomon <salomonyaacov@gmail.com>
2020-03-05 00:29:10 +0200

    heroku domain verification

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,27 +7,27 @@ module.exports = (app) => {
     app.get('/google88a4f773616f82b9.html',(req,res)=>{
         res.sendFile(path.join(__filename+'/../../google88a4f773616f82b9.html'))
     })
-    // app.get('/auth/google',
-    //     passport.authenticate('google', {
-    //         scope: ['profile', 'email']
-    //     }))
-    // //put user on hold and take the 'code' from the url and exchanging it for profile info
-    // app.get('/auth/google/callback', passport.authenticate('google'))
+    app.get('/auth/google',
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
+        }))
+    //put user on hold and take the 'code' from the url and exchanging it for profile info
+    app.get('/auth/google/callback', passport.authenticate('google'))
     
-    // app.get('/auth/facebook',
-    //     passport.authenticate('facebook', {
-    //         scope:['user_friends'],
-    //     }))
-    // app.get('/auth/facebook/callback', passport.authenticate('facebook'))    
+    app.get('/auth/facebook',
+        passport.authenticate('facebook', {
+            scope:['user_friends'],
+        }))
+    app.get('/auth/facebook/callback', passport.authenticate('facebook'))    
 
-    // app.get('/api/logout', (req, res)=>{
-    //     req.logout();
-    //     res.send(req.user)
-    // })
+    app.get('/api/logout', (req, res)=>{
+        req.logout();
+        res.send(req.user)
+    })
 
-    // app.get('/api/current_user', (req, res)=>{
-    //     res.send(req.user);
-    // })
+    app.get('/api/current_user', (req, res)=>{
+        res.send(req.user);
+    })
 };
 
 //, 'manage_pages'
--- services/passportService.js
+++ services/passportService.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const GoogleStratgey = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const mongoose = require('mongoose')
-//const keys = require('../config/keys')
+const keys = require('../config/keys')
 
 const User = mongoose.model('user');
 
@@ -18,43 +18,43 @@ passport.deserializeUser((id, done) => {
     })
 })
 
-// //defining the google stratgey & adding user to database
-// passport.use(new GoogleStratgey({
-//     clientID: keys.googleClientID,
-//     clientSecret: keys.googleClientSecret,
-//     callbackURL: '/auth/google/callback'
-// },
-//     (accessToken, refreshToken, profile, done) => {
-//         User.findOne({ googleID: profile.id })
-//             .then((existingUser) => {
-//                 if (existingUser) {
-//                     done(null, existingUser)
-//                 } else {
-//                     new User({ googleID: profile.id }).save()
-//                         .then(user => {
-//                             done(null, user)
-//                         })
-//                 }
-//             })
-//     })
-// );
+//defining the google stratgey & adding user to database
+passport.use(new GoogleStratgey({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+},
+    (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleID: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    done(null, existingUser)
+                } else {
+                    new User({ googleID: profile.id }).save()
+                        .then(user => {
+                            done(null, user)
+                        })
+                }
+            })
+    })
+);
 
-// passport.use(new FacebookStrategy({
-//     clientID: keys.facebookClientID,
-//     clientSecret: keys.facebookClientSecret,
-//     callbackURL: '/auth/facebook/callback'
-// },
-//     (accessToken, refreshToken, profile, done) => {
-//        User.findOne({facebookID: profile.id})
-//        .then((existingUser)=>{
-//            if (existingUser) {
-//                done(null, existingUser)
-//            } else {
-//                new User({facebookID: profile.id}).save()
-//                .then(user=>{
-//                    done(null, user)
-//                })
-//            }
-//        })
-//     })
-// )
+passport.use(new FacebookStrategy({
+    clientID: keys.facebookClientID,
+    clientSecret: keys.facebookClientSecret,
+    callbackURL: '/auth/facebook/callback'
+},
+    (accessToken, refreshToken, profile, done) => {
+       User.findOne({facebookID: profile.id})
+       .then((existingUser)=>{
+           if (existingUser) {
+               done(null, existingUser)
+           } else {
+               new User({facebookID: profile.id}).save()
+               .then(user=>{
+                   done(null, user)
+               })
+           }
+       })
+    })
+)
